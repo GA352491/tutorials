1b2e60be576edce5befe568cdcefea2e86264dd3
Anish <59597772+GA352491@users.noreply.github.com>
2021-04-10 17:24:44 +0530

    Update settings.py

--- tutorials/settings.py
+++ tutorials/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'mrlxp6=g5$)5pdc+)+zg+=wifqfvz1o83#0jksrg$j^lfregg^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tutorialz.herokuapp.com']
 
 # Application definition
 
