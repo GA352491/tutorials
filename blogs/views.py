b5399b13fb0e8966f2f1a82634fe6c1f6d0dae35
Anish <59597772+GA352491@users.noreply.github.com>
2021-04-10 16:46:28 +0530

    Add files via upload

---
+++ blogs/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from .models import Course, Category, TutorialBlog
+
+from django.template.loader import render_to_string
+
+import random
+
+
+# Create your views here.
+
+def home(request):
+    courses = Course.objects.all()
+    a = [
+        ' background: radial-gradient(53.85% 64.23% at 79.27% 51.46%, #100101 28.38%, rgba(34, 3, 3, 0) 100%), #440606;',
+        'background:radial-gradient(53.85% 64.23% at 79.27% 51.46%, #100101 28.38%, rgba(34, 3, 3, 0) 100%), #440606;',
+
+        'background-image: linear-gradient(160deg,  #590202 0%,#0D0D0D 0%,#010440  );',
+        'background: radial-gradient(81.91% 68.48% at 12.19% 74.98%, #808080 0%, #808080 0%, rgba(255, 255, 255, 0) 81.37%), #000000;',
+    ]
+    b = random.choice(a)
+
+
+    context = {'courses': courses, 'b': b}
+    return render(request, 'home/home.html', context)
+
+
+def tutorial(request, pk):
+    course = Course.objects.get(id=pk)
+    # print(course.id)
+    category = Category.objects.filter(course=course)
+    t = TutorialBlog.objects.filter(course=course.id).first()
+    t2 = TutorialBlog.objects.filter(course=course.id).first()
+    # print(t2)
+    blogs = TutorialBlog.objects.filter(course=course)
+    # print(blogs)
+
+    context = {'category': category, 't': t, 'blogs': blogs, 'course': course, }
+    return render(request, 'snippets/sidebar/index (1).html', context)
+
+
+def tutorial_view(request, pk):
+    blog = TutorialBlog.objects.get(id=pk)
+    course = blog.course
+    category = Category.objects.filter(course=course)
+    blogs = TutorialBlog.objects.filter(course=course)
+
+    context = {'blog': blog, 'category': category, 'blogs': blogs, 'course': course, }
+
+    return render(request, 'tutorial/tutorial_view.html', context)
+
+
+def test(request):
+    return render(request, 'test.html')
