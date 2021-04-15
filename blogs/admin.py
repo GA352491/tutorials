8372fa458d0b727210ae4d13ebb62094b62f1a30
Anish <59597772+GA352491@users.noreply.github.com>
2021-04-15 11:42:25 +0530

    Delete blogs directory

--- blogs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Course, Category, TutorialBlog,Images
-
-# Register your models here.
-
-admin.site.register(Course)
-admin.site.register(Category)
-admin.site.register(TutorialBlog)
-admin.site.register(Images)
